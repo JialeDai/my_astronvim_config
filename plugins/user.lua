c1f3ae3ab74385d6a165b80a5f768a20d28fdfe6
JialeDai <terry.jdai@gmail.com>
2024-03-09 23:22:51 -0800

    add hov plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,19 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "smoka7/hop.nvim",
+    opts = {},
+    event = "User AstroFile",
+    keys = {
+      { "w", "<cmd>HopWord<cr>", desc = "HopWord" },
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+  },
 }
